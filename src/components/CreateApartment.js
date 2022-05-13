6b694ae7d82b432b3e8fcb6fb265df94273f1e7e
Louise <louisegabriellecc@gmail.com>
2022-05-13 11:35:41 +0100

    Iron BnB - Display apartments in API

---
+++ src/components/CreateApartment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useState} from 'react';
+
+const CreateApartment = () => {
+
+
+  return (
+    <div>
+      <form>
+
+      </form>
+    </div>
+  )
+}
+
+export default CreateApartment
